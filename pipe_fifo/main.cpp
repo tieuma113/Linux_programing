c28227b18b177e724cec6b1f2b841ec696fcb64f
Khanh <dk.assassins.9x@gmail.com>
2025-06-05 23:10:01 +0700

    pipe_fifo ec5 6

--- pipe_fifo/main.cpp
+++ pipe_fifo/main.cpp
@@ -2,8 +2,10 @@
 #include <cerrno>
 #include <cstdio>
 #include <cstring>
+#include <fcntl.h>
 #include <iostream>
 #include <sys/select.h>
+#include <sys/types.h>
 #include <unistd.h>
 #include <cstdlib>
 #include <sys/wait.h>
@@ -275,7 +277,97 @@ void ec4(){
 // Chạy process reader trước, mở FIFO ở O_RDONLY, rồi sleep vài giây
 // Sau đó mới chạy writer
 // Quan sát xem reader có bị block không → giải thích vì sao
-void ec5(){}
+void ec5(){
+    std::string path = "/tmp/myfifo";
+    std::cout << "Make fifo" << std::endl;
+    if(mkfifo(path.c_str(), 0666) == -1 && errno != EEXIST){
+        std::cerr << "make fifo fail" << std::endl;
+        return;
+    }
+    //make output pipe
+    int pipe_read[2];
+    int pipe_write[2];
+    if(pipe(pipe_read) == -1 || pipe(pipe_write) == -1){
+        std::cout << "Fail to create pipe" << std::endl;
+    }
+    int pid1 = fork();
+    if (pid1 < 0){
+        std::cerr << "Fail to open reader process" << std::endl;
+        return;
+    }else if(pid1 == 0){
+        //reader process
+        //redirect output stream
+        close(pipe_read[0]);
+        close(pipe_write[0]);
+        close(pipe_write[1]);
+        dup2(pipe_read[1], STDOUT_FILENO);
+        close(pipe_read[1]);
+        char* args[] = {(char*)"./build/read", nullptr};
+        std::cout << "[READER] TRY OPEN" << std::endl;
+        execvp(args[0], args);
+        std::cerr << "Fail to open read binary" << std::endl;
+        _exit(1);
+    }
+
+    int pid2 = fork();
+    if(pid2 < 0){
+        std::cerr << "Fail to open write process" << std::endl;
+        return;
+    }
+    else if (pid2 == 0) {
+        close(pipe_read[0]);
+        close(pipe_read[1]);
+        close(pipe_write[0]);
+        dup2(pipe_write[1], STDOUT_FILENO);
+        close(pipe_write[1]);
+        sleep(10);
+        char* args[] = {(char*)"./build/write", nullptr};
+        std::cout << "[WRITER] TRY OPEN" << std::endl;
+        execvp(args[0], args);
+        std::cerr << "fail to open write binary" << std::endl;
+        _exit(1);
+    }
+
+    //main process
+    close(pipe_read[1]);
+    close(pipe_write[1]);
+    char buf[256];
+    fd_set readfds;
+    int maxfd = std::max(pipe_read[0], pipe_write[0]) + 1;
+    while (true)
+    {    
+        FD_ZERO(&readfds);
+        FD_SET(pipe_read[0], &readfds);
+        FD_SET(pipe_write[0], &readfds);
+
+        int ready = select(maxfd, &readfds, nullptr, nullptr, nullptr);
+        if (ready == -1){
+            std::cerr << "select fail" << std::endl;
+            break;
+        }
+        if(FD_ISSET(pipe_read[0], &readfds)){
+            ssize_t n = read(pipe_read[0], buf, sizeof(buf) - 1);
+            std::cout << "[READ] n = " << n << std::endl;
+            if (n <= 0) break;
+            buf[n] = '\0';
+            std::cout << buf;
+        }
+        if(FD_ISSET(pipe_write[0], &readfds)){
+            ssize_t n = read(pipe_write[0], buf, sizeof(buf) - 1);
+            std::cout << "[WRITE] n = " << n << std::endl;
+            if(n <= 0) break;
+            buf[n] = '\0';
+            std::cout << buf;
+        }
+    }
+    close(pipe_read[0]);
+    close(pipe_write[0]);
+
+    waitpid(pid1, nullptr, 0);
+    waitpid(pid2, nullptr, 0);
+    unlink(path.c_str());
+    return;
+}
 
 //  Bài 6 – Nhiều tiến trình ghi vào FIFO
 // Mục tiêu: Quan sát race condition.
@@ -284,7 +376,67 @@ void ec5(){}
 //  3 tiến trình ghi "Hello from A/B/C" cùng lúc
 //  Tiến trình đọc in toàn bộ dữ liệu nhận được
 // Câu hỏi phụ: Nếu không đồng bộ, dữ liệu bị trộn không?
-void ec6(){}
+void ec6(){
+    std::string path = "/tmp/myfifo";
+    if(mkfifo(path.c_str(), 0666) == -1 && errno != EEXIST){
+        std::cerr << "Fail to create fifo" << std::endl;
+    }
+    std::cout << "Create process 1" << std::endl;
+    int pid1 = fork();
+    if(pid1 < 0){
+        std::cerr << "fork faiil" << std::endl;
+        return;
+    }
+    else if (pid1 == 0) {
+        std::cout << "Success creat process 1" << std::endl;
+        int fd = open(path.c_str(), O_WRONLY | O_NONBLOCK);
+        std::string msg = "[PROCESS1] Hello from A";
+        sleep(3);
+        write(fd, msg.c_str(), msg.size());
+        close(fd);
+        _exit(1);
+    }
+    int pid2 = fork();
+    if(pid2 < 0){
+        std::cerr << "fork fail" << std::endl;
+        return;
+    }
+    else if (pid2 == 0) {
+        std::cout << "Success create process 2" << std::endl;
+        int fd = open(path.c_str(), O_WRONLY | O_NONBLOCK);
+        std::string msg = "[PROCESS2] Hello from B";
+        sleep(3);
+        write(fd, msg.c_str(), msg.size());
+        close(fd);
+        _exit(1);
+    }
+    int pid3 = fork();
+    if(pid3 < 0){
+        std::cerr << "fork fail" << std::endl;
+        return;
+    }
+    else if (pid3 == 0) {
+        std::cout << "Success create process 3" << std::endl;
+        int fd = open(path.c_str(), O_WRONLY | O_NONBLOCK);
+        std::string msg = "[PROCESS3] Hello from C";
+        sleep(3);
+        write(fd, msg.c_str(), msg.size());
+        close(fd);
+        _exit(1);
+    }
+    //parent
+    int fd = open(path.c_str(), O_RDONLY);
+    char buf[100];
+    ssize_t n = read(fd, buf, sizeof(buf));
+    if(n>0){
+        std::cout << "received: " << buf;
+    }
+    close(fd);
+    waitpid(pid1, nullptr, 0);
+    waitpid(pid2, nullptr, 0);
+    waitpid(pid3, nullptr, 0);
+    unlink(path.c_str());
+}
 
 int main(){
     #ifdef EC1
@@ -301,6 +453,14 @@ int main(){
 
     #ifdef EC4
     ec4();
+    #endif
+
+    #ifdef EC5
+    ec5();
+    #endif
+
+    #ifdef EC6
+    ec6();
     #endif
     return 0;
 }
